f780d8cbb202fc3a2ceefb50d9da5fe799f6f666
daviddawha <daviddawha@gmail.com>
2015-10-14 02:02:21 -0400

    Create ForDemo.java

---
+++ ForDemo.java
@@ -0,0 +1,16 @@
+class ForDemo {
+    public static void main(String[] args){
+    int mobile = 3;
+    String[] service = {"High","Low","Out of Range"};
+    
+         if(mobile == 1){
+            System.out.println(service[0]);
+         }
+         else if (mobile == 2){
+         	System.out.println(service[1]);
+         }
+         else {
+         System.out.println(service[2]);
+         }
+    }
+}
